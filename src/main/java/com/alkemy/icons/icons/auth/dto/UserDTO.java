d09ff7dbe8ac6af983cea70fe3c8eda309d2226b
Marisa Caponetto <ing.mcaponetto@gmail.com>
2021-11-25 12:08:01 -0300

    Security first part

---
+++ src/main/java/com/alkemy/icons/icons/auth/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.alkemy.icons.icons.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    
+    private String username;
+    private String password;
+    
+}
